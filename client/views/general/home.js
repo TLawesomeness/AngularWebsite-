31b91f96cf2f1ea89c87fbc108b53b3c5d2b1823
Tommyluo <tluo12321@yahoo.com>
2015-04-07 00:44:28 -0700

    added contact form, fixed image bugs

--- client/views/general/home.js
+++ client/views/general/home.js
@@ -4,9 +4,9 @@ angular.module('myWeb')
 
 .controller('HomeCtrl', ['$scope', function($scope) {
   $('.homeimage').mouseover(function () {
-    $(this).attr("src", "images/myfunnypic.png");
+    $(this).attr("src", "/views/images/myfunnypic.png");
   })
   .mouseout(function () {
-    $(this).attr("src", "images/mynewpic.png");
+    $(this).attr("src", "/views/images/mynewpic.png");
   });
 }]);
--- client/views/nav/nav.js
+++ client/views/nav/nav.js
@@ -1,21 +1,10 @@
-// 'use strict';
-//
-// angular.module('myWeb')
-//
-// .controller('NavCtrl', ['$scope', function($scope) {
-//   // $('#contactbtn').click(function() {
-//   //   $('#myModal').modal();
-//   // });
-//   //
-//   // $scope.contact = function() {
-//   //   $('#myModal').modal();
-//   // };
-//
-//   // $scope.contact = function() {
-//   //   var modal = $modal.open({
-//   //     templateUrl: 'contact.html',
-//   //     controller: 'NavCtrl'
-//   //   });
-//   // };
-//
-// }]);
+'use strict';
+
+angular.module('myWeb')
+
+.controller('NavCtrl', ['$scope', function($scope) {
+
+  $scope.contact = function() {
+    $('#myModal').modal();
+  };
+}]);
